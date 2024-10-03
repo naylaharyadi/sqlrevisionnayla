93632e0a4ab5e8ec11b5bd6693841d4ef7328f28
naylaharyadi <haryadinayla@outlook.Com>
2024-10-03 14:50:42 +0200

    6

--- exo1.sql
+++ exo1.sql
@@ -9,3 +9,5 @@ select nom from clients where location = 'Paris' and age between 25 and 35,
 select nom_campagne from campagnes order by budget, 
 
 
+select nom_campagne from campagnes order by debut_campagne
+limit 5, 
