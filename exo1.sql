2e3bebdf7abe27244d68abd5177a7f7f1a2f80e4
naylaharyadi <haryadinayla@outlook.Com>
2024-10-03 15:02:41 +0200

    8

--- exo1.sql
+++ exo1.sql
@@ -1,18 +1,18 @@
-select nom FROM clients,
+select nom FROM Clients,
 
-select nom, email FROM clients, 
+select nom, email FROM Clients, 
 
-select nom from clients where age > 40,
+select nom from Clients where age > 40,
 
-select nom from clients where location = 'Paris' and age between 25 and 35,
+select nom from Clients where location = 'Paris' and age between 25 and 35,
 
-select nom_campagne from campagnes order by budget, 
+select nom_campagne from Campagnes order by budget, 
 
 
-select nom_campagne from campagnes order by debut_campagne
+select nom_campagne from Campagnes order by debut_campagne
 limit 5, 
 
-select count (*) from campagnes,
+select count (*) from Campagnes,
 
-select avg (age) from clients,
+select avg (age) from Clients,
 
